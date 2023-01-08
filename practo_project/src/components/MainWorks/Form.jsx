703b0d20962c21e07adfab17e9cf9f64a07ddfca
UttamMehta <binobajichiku@gmail.com>
2023-01-08 17:38:12 +0530

    All pages Add

---
+++ practo_project/src/components/MainWorks/Form.jsx
@@ -0,0 +1,51 @@
+// import Styles from "../Routes/checkout.module.css"
+
+import React, { useState } from 'react';
+
+function Form() {
+
+    const [userCard,setUserCard]
+=useState({
+    name:"",
+    card:"",
+    exp:"",
+    cvc:"",
+    pin:"",
+})
+
+const user=(e)=>{
+    const {name,value}=e.target;
+
+    setUserCard({...userCard,name:value})
+}
+
+    return (
+        <>
+             <h2 style={{paddingTop:"12px",marginTop:"0px"}}>Card details</h2>
+        <h3 id="amount"></h3>
+        <input type="text" name="name" id="name" placeholder="Enter Your Name" onChange={user}/><br />
+        <input type="number" name="card" id="card" placeholder="Enter Card Number" onChange={user}/><br />
+        <input type="text" name="exp" id="exp" placeholder="Expiration date (MM/YYYY)" onChange={user}/>
+        <input type="password" name="cvc" id="cvc" placeholder="CVC/CVV" onChange={user}/>
+        <input type="password" placeholder="Enter your pin" id="pass" name="pin"onChange={user}/><br />
+        <button disabled={
+
+userCard.name&&userCard.card.length==16&&userCard.exp&&userCard.cvc.length==3&&userCard.pin>=4&&userCard.pin<=6?true:false
+
+
+        } style={{ margin:"0px",
+                padding:"0px", backgroundColor: "cornflowerblue",
+                marginBottom: "20px",  alignItems: "center",
+                fontWeight: "bold",
+                width: "90%",
+                borderRadius: "8px",
+                backgroundColor:"#e7e7e7",
+                cursor: "pointer", backgroundColor: "cornflowerblue",
+                marginBottom: "20px",height:"30px",marginTop:"10px",fontSize:"medium"}}
+                >Submit</button>
+            
+        </>
+    );
+}
+
+export default Form;
